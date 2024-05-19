06a9e04a9316a38236dc2de951dcfe49ee32d1a8
PaulMarmet <paulmarmet1002@gmail.com>
2024-05-19 18:29:28 -0400

    Added explosion_proof tag doing the equivalent to fire_proof :)

--- src/main/java/net/paulm/hacksaw/item/HacksawItemTags.java
+++ src/main/java/net/paulm/hacksaw/item/HacksawItemTags.java
@@ -6,7 +6,8 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 public class HacksawItemTags {
-    public static final TagKey<Item> FIREPROOF = TagKey.of(RegistryKeys.ITEM, new Identifier("hacksaw", "fireproof"));
+    public static final TagKey<Item> FIREPROOF = TagKey.of(RegistryKeys.ITEM, new Identifier("hacksaw", "fire_proof"));
+    public static final TagKey<Item> EXPLOSION_PROOF = TagKey.of(RegistryKeys.ITEM, new Identifier("hacksaw", "explosion_proof"));
 
     public static void  registerModTags() {
 
--- src/main/java/net/paulm/hacksaw/mixin/ItemImmuneExplosionMixin.java
+++ src/main/java/net/paulm/hacksaw/mixin/ItemImmuneExplosionMixin.java
@@ -1,19 +1,24 @@
 package net.paulm.hacksaw.mixin;
 
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.item.ItemStack;
 import net.paulm.hacksaw.HacksawConfig;
+import net.paulm.hacksaw.item.HacksawItemTags;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ItemEntity.class)
-public class ItemImmuneExplosionMixin extends ImmuneExplosionMixin{
+public abstract class ItemImmuneExplosionMixin extends ImmuneExplosionMixin{
+
+	@Shadow public abstract ItemStack getStack();
 
 	@Override
 	public void isImmuneToExplosion(CallbackInfoReturnable<Boolean> cir) {
 		if (HacksawConfig.explosionProofItems == HacksawConfig.SelectionType.ALL) {
 			cir.setReturnValue(true);
 		} else if (HacksawConfig.explosionProofItems == HacksawConfig.SelectionType.SOME) {
-			cir.setReturnValue(true);
+			cir.setReturnValue(cir.getReturnValue() || this.getStack().isIn(HacksawItemTags.EXPLOSION_PROOF));
 		}
 	}
 
