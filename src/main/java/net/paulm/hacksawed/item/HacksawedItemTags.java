baeb4ad0d58c03d4a23594494af729218818d778
PaulMarmet <paulmarmet1002@gmail.com>
2024-05-27 17:06:24 -0400

    Added a tag for items that you can place on your head without angering endermen. Fixed the tag names in the config

--- src/main/java/net/paulm/hacksawed/item/HacksawedItemTags.java
+++ src/main/java/net/paulm/hacksawed/item/HacksawedItemTags.java
@@ -8,6 +8,7 @@ import net.minecraft.util.Identifier;
 public class HacksawedItemTags {
     public static final TagKey<Item> FIREPROOF = TagKey.of(RegistryKeys.ITEM, new Identifier("hacksawed", "fire_proof"));
     public static final TagKey<Item> EXPLOSION_PROOF = TagKey.of(RegistryKeys.ITEM, new Identifier("hacksawed", "explosion_proof"));
+    public static final TagKey<Item> ENDERMAN_SAFE = TagKey.of(RegistryKeys.ITEM, new Identifier("hacksawed", "enderman_safe"));
 
     public static void  registerModTags() {
 
