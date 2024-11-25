1e537eb7bbdecd8bb8ec6b9aaab841113b436ee9
PaulMarmet <paulmarmet1002@gmail.com>
2024-11-24 21:55:38 -0500

    Negative Feature Update
    Removed all the vanilla changes and moved them over to Stitched
    Changed license

--- src/main/java/net/pm/hacksawed/HacksawedConfig.java
+++ src/main/java/net/pm/hacksawed/HacksawedConfig.java
@@ -30,29 +30,6 @@ public class HacksawedConfig extends MidnightConfig {
     @Entry(category = "general", min = 0f) public static float bouncyBallDrag = 0.7f;
     @Entry(category = "general", min = 0f) public static float returnalOrbVelTransfer = 0.4f;
 
-    //Item Changes Configs
-    @Comment(category = "general") public static Comment ItemConfig1;
-    @Entry(category = "general") public static SelectionType explosionProofItems = SelectionType.ALL;
-    @Entry(category = "general") public static boolean explosionProofBlastProt = true;
-    @Entry(category = "general") public static SelectionType fireProofItems = SelectionType.SOME;
-    @Entry(category = "general") public static boolean fireProofFireProt = true;
-
-    //Other Configs
-    @Comment(category = "general") public static Comment OtherConfig1;
-    @Entry(category = "general") public static boolean untramplableWetFarmland = true;
-    @Entry(category = "general") public static boolean extendWaterRegion = true;
-    @Entry(category = "general") public static boolean echoShardRings = false;
-    @Entry(category = "general") public static boolean glowingTorchflower = true;
-    @Entry(category = "general") public static boolean blocksReleaseSparks = true;
-    @Entry(category = "general") public static boolean iceAndSnowDontMelt = true;
-    @Entry(category = "general") public static boolean moreEndermanSafeItems = true;
-    @Entry(category = "general") public static boolean sneakSweetBerries = true;
-    @Entry(category = "general") public static boolean bundleChanges = true;
-    @Entry(category = "general", min = 0, max = 64) public static int bundleMaxSpaceCost = 8;
-    @Entry(category = "general") public static boolean copperGrateFallthrough = true;
-    @Entry(category = "general", min = 0.0, max = 256) public static double copperGrateFallthroughSize = 0.7;
-    @Entry(category = "general") public static boolean drowningDrops = true;
-    @Entry(category = "general") public static boolean regrowCaveVines = true;
 
     public static void addToConditions() {
         HacksawedConditions.addConfigToDict("hacksaw_enabled", hacksawEnabled);
